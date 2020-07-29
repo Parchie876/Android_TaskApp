bf944f24ec585c7336c81800a4b22f4051419416
Dillon Parchment <dillonparchmentkool@gmail.com>
2020-07-28 19:51:09 -0500

    User can create new task

--- app/src/main/java/com/example/tasklist/NewTaskActivity.java
+++ app/src/main/java/com/example/tasklist/NewTaskActivity.java
@@ -1,17 +1,35 @@
 package com.example.tasklist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
 
 public class NewTaskActivity extends AppCompatActivity {
     TextView titlePage, addTitle, addDescription,addDate;
     EditText titleTask, taskDescription, taskTargetDate;
     Button   btnSaveTask, btnCancelTask;
+    DatabaseReference databaseReference;
+
+    TaskAdapter taskAdapter;
+    Integer taskNum = new Random().nextInt();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +47,31 @@ public class NewTaskActivity extends AppCompatActivity {
 
         btnSaveTask = findViewById(R.id.btnSaveTask);
         btnCancelTask = findViewById(R.id.btnCancelTask);
+
+        btnSaveTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // insert data to database
+                databaseReference = FirebaseDatabase.getInstance().getReference().child("TaskList").
+                        child("Task"+taskNum);
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        dataSnapshot.getRef().child("taskTitle").setValue(titleTask.getText().toString());
+                        dataSnapshot.getRef().child("taskDesc").setValue(taskDescription.getText().toString());
+                        dataSnapshot.getRef().child("taskDate").setValue(taskTargetDate.getText().toString());
+
+                        Intent a = new Intent( NewTaskActivity.this,MainActivity.class);
+                        startActivity(a);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
 
         /*
         import @fonts */
